b67fa115a7387987eee26bee5a7fd360070d96c7
abudy <abudy@gmail.com>
2024-07-17 18:33:02 -0400

    created migration and bug fixes

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Check_List(models.Model):
-    task = models.CharField(max_lenfth=50)
+    task = models.CharField(max_length=50)
     description = models.TextField()
     status = models.BooleanField(default = False)
 
